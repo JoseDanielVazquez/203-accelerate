95fdcf02522e3c4e1b14d70ef9bd7c2204974dd9
JoseDanielMX <jduvmx@gmail.com>
2021-07-02 19:19:21 -0500

    Added featured work to the homepage

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -22,6 +22,30 @@ get_header(); ?>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</section><!-- #primary -->
+
+	<section class="featured-work">
+		<div class="site-content">
+			<h4>Featured Work</h4>
+			<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+				<?php while ( have_posts() ) : the_post(); 
+					$image_1 = get_field("image_1");
+					$medium_size = "medium";
+				?>
+				<li class="individual-featured-work">
+					<figure>
+						<?php if($image_1) {
+							echo wp_get_attachment_image( $image_1, $medium_size );
+						} ?>
+					</figure>
+					<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
+				<li>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
+			</ul>
+		</div>
+	</section>
+
 	<section class="recent-posts">
 		<div class="site-content">
 			<div class="blog-post">
