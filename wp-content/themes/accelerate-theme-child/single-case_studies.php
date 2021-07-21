70820105a4b0eeb3e0a177c7ae003918c27f0354
JoseDanielMX <jduvmx@gmail.com>
2021-07-21 18:31:15 -0500

    Fixed a comment

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
+ * The template for displaying a single case study page
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
