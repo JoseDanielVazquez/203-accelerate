ad052873f88c60d996461aa76607534009854814
JoseDanielMX <jduvmx@gmail.com>
2021-07-25 00:08:29 -0500

    Fixed padding of the newsletter container

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -64,7 +64,7 @@ function accelerate_theme_child_widget_init() {
     'name' =>__( 'Blogpage sidebar', 'accelerate-theme-child'),
     'id' => 'sidebar-3',
     'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
-    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'before_widget' => '<aside id="%1$s" class="newsletter-container %2$s">',
         'after_widget' => '</aside>',
     'before_title' => '<h3 class="widget-title">',
         'after_title' => '</h3>',
